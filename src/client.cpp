9f30428c993a38752e3708a5bf34af745a2e1a6d
Anatoliy <and1life@yandex.ru>
2025-12-18 16:16:40 +0300

    feat(Client): added implementation  send_message()

--- src/client.cpp
+++ src/client.cpp
@@ -8,3 +8,18 @@ TCPClient::TCPClient(boost::asio::io_context &io_context, const std::string &hos
 
     boost::asio::connect(socket_, endpoints);
 }
+
+void TCPClient::send_message(const std::string &message)
+{
+    boost::asio::write(socket_, boost::asio::buffer(message + '\n'));
+
+    boost::asio::streambuf buf;
+
+    boost::asio::read_until(socket_, buf, '\n');
+
+    std::istream is(&buf);
+    std::string response;
+    std::getline(is, response);
+
+    std::cout << "Response from the server: " << response << std::endl;
+}
