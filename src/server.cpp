e94fca66eb06b7aab6925bb0abe13e437dd73d15
Anatoliy <and1life@yandex.ru>
2025-12-18 03:51:26 +0300

    feat(Server): added implementation start()

--- src/server.cpp
+++ src/server.cpp
@@ -2,3 +2,9 @@
 
 TCPServer::TCPServer(boost::asio::io_context &io_context, short port) : acceptor_(io_context,
      boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), port)) {}
+
+void TCPServer::start()
+{
+    std::cout << "The server is running. Waiting for connection..." << std::endl;
+    accept_connection();
+}
