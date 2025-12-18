e71191b6d6418c87959fe0f02c278e3c9d64bf1d
Anatoliy <and1life@yandex.ru>
2025-12-18 04:01:05 +0300

    feat(Server): added implementation accept_connection()

--- src/server.cpp
+++ src/server.cpp
@@ -8,3 +8,25 @@ void TCPServer::start()
     std::cout << "The server is running. Waiting for connection..." << std::endl;
     accept_connection();
 }
+
+void TCPServer::accept_connection()
+{
+    boost::asio::ip::tcp::socket socket(acceptor_.get_executor().context());
+
+    acceptor_.accept(socket);
+    std::cout << "The client has connected!" << std::endl;
+
+    boost::asio::streambuf buf;
+
+    boost::asio::read_until(socket, buf, '\n');
+
+    std::istream is(&buf);
+    std::string message;
+    std::getline(is, message);
+
+    std::cout << "Message received: " << message << std::endl;
+
+    boost::asio::write(socket, boost::asio::buffer("Message received!\n"));
+
+    accept_connection();
+}
