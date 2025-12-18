300b6add58c26f9ac682eefb12834007e0479d18
Anatoliy <and1life@yandex.ru>
2025-12-18 16:20:22 +0300

    feat(Client): added implementation client startup

---
+++ apps/client_app.cpp
@@ -0,0 +1,18 @@
+#include "client.hpp"
+#include <iostream>
+
+int main()
+{
+    boost::asio::io_context io_context;
+
+    TCPClient client(io_context, "127.0.0.1", 1234);
+
+    std::cout << "Print message: ";
+    std::string message;
+    std::getline(std::cin, message);
+
+    client.send_message(message);
+
+
+    return 0;
+}
