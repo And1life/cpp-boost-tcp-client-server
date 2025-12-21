31d866122e92ea2ab08835fdfd3a10088e5b436a
Anatoliy <and1life@yandex.ru>
2025-12-21 20:42:58 +0300

    fix(Client): updated Boost.Asio API to resolver.resolve() (1.70+)

--- src/client.cpp
+++ src/client.cpp
@@ -3,8 +3,7 @@
 TCPClient::TCPClient(boost::asio::io_context &io_context, const std::string &host, short port)
     : socket_(io_context), resolver_(io_context)
 {
-    boost::asio::ip::tcp::resolver::query query(host, std::to_string(port));
-    boost::asio::ip::tcp::resolver::results_type endpoints = resolver_.resolve(query);
+    boost::asio::ip::tcp::resolver::results_type endpoints = resolver_.resolve(host, std::to_string(port));
 
     boost::asio::connect(socket_, endpoints);
 }
